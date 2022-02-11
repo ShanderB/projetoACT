b3b3785578db32f79bb440370794b13772364208
ShanderB <s.hander8910@hotmail.com>
2022-02-10 22:32:12 -0300

    refac

--- js/factory/factory.js
+++ js/factory/factory.js
@@ -3,29 +3,34 @@ const factoryName = 'ComicBooks'
 const factory = function ($http, $q) {
   const publicKey = '5a237863b3cc2061003cbbc4fe20dc06'
   const baseUrl = 'http://gateway.marvel.com/v1/'
+  const hash = 'bd4b447a65ef5d6b174f87cf9db6d2db'
+  const query = '&apikey=' + publicKey + '&hash=' + hash + '&ts=1'
   const limit = 50
 
   const find = function () {
     const def = $q.defer()
-    const url = baseUrl + 'public/characters?limit=' + limit + '&apikey=' + publicKey + '&hash=bd4b447a65ef5d6b174f87cf9db6d2db&ts=1'
+    const url = baseUrl + 'public/characters?limit=' + limit + query
     $http.get(url).success(def.resolve).error(def.reject)
 
     return def.promise
   }
+
   const findOne = function (id) {
     const def = $q.defer()
-    const url = baseUrl + 'public/characters/' + id + '?apikey=' + publicKey + '&hash=bd4b447a65ef5d6b174f87cf9db6d2db&ts=1'
+    const url = baseUrl + 'public/characters/' + id + query.replace('&', '?')
     $http.get(url).success(def.resolve).error(def.reject)
 
     return def.promise
   }
+
   const findNext = function (offset) {
     const def = $q.defer()
-    const url = baseUrl + 'public/characters?limit=' + limit + '&offset=' + (limit * offset) + '&apikey=' + publicKey + '&hash=bd4b447a65ef5d6b174f87cf9db6d2db&ts=1'
+    const url = baseUrl + 'public/characters?limit=' + limit + '&offset=' + (limit * offset) + query
     $http.get(url).success(def.resolve).error(def.reject)
 
     return def.promise
   }
+
   const LoadMore = function ($scope) {
     this.offset = 0
     this.busy = false
