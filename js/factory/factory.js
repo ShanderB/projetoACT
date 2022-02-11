e48e19a5737368f2868bbd44835dfa046d5fb8d4
ShanderB <s.hander8910@hotmail.com>
2022-02-10 22:02:23 -0300

    refact

--- js/factory/factory.js
+++ js/factory/factory.js
@@ -1,57 +1,56 @@
 const factoryName = 'ComicBooks'
 
+const factory = function ($http, $q) {
+  const publicKey = '5a237863b3cc2061003cbbc4fe20dc06'
+  const baseUrl = 'http://gateway.marvel.com/v1/'
+  const limit = 50
 
-const factory = function($http,$q) {
-	const publicKey = '5a237863b3cc2061003cbbc4fe20dc06';
-	const baseUrl = 'http://gateway.marvel.com/v1/';
-	const limit = 50;
+  const find = function () {
+    const def = $q.defer()
+    const url = baseUrl + 'public/characters?limit=' + limit + '&apikey=' + publicKey + '&hash=bd4b447a65ef5d6b174f87cf9db6d2db&ts=1'
+    $http.get(url).success(def.resolve).error(def.reject)
 
-	const find = function() {
-		var def = $q.defer();
-		var url = baseUrl + 'public/characters?limit='+ limit +'&apikey=' + publicKey + '&hash=bd4b447a65ef5d6b174f87cf9db6d2db&ts=1';
-		$http.get(url).success(def.resolve).error(def.reject);
+    return def.promise
+  }
+  const findOne = function (id) {
+    const def = $q.defer()
+    const url = baseUrl + 'public/characters/' + id + '?apikey=' + publicKey + '&hash=bd4b447a65ef5d6b174f87cf9db6d2db&ts=1'
+    $http.get(url).success(def.resolve).error(def.reject)
 
-		return def.promise;
-	};
-	const findOne = function(id) {
-		var def = $q.defer();
-		var url = baseUrl + 'public/characters/' + id +'?apikey=' + publicKey + '&hash=bd4b447a65ef5d6b174f87cf9db6d2db&ts=1';
-		$http.get(url).success(def.resolve).error(def.reject);
+    return def.promise
+  }
+  const findNext = function (offset) {
+    const def = $q.defer()
+    const url = baseUrl + 'public/characters?limit=' + limit + '&offset=' + (limit * offset) + '&apikey=' + publicKey + '&hash=bd4b447a65ef5d6b174f87cf9db6d2db&ts=1'
+    $http.get(url).success(def.resolve).error(def.reject)
 
-		return def.promise;
-	};
-	const findNext = function(offset) {
-		var def = $q.defer();
-		var url = baseUrl + 'public/characters?limit=' + limit +'&offset=' + (limit*offset) + '&apikey=' + publicKey + '&hash=bd4b447a65ef5d6b174f87cf9db6d2db&ts=1';
-		$http.get(url).success(def.resolve).error(def.reject);
+    return def.promise
+  }
+  const LoadMore = function ($scope) {
+    this.offset = 0
+    this.busy = false
+    this.characters = []
+    this.load = function () {
+      if (this.busy) {
+        return
+      }
+      this.busy = true
+      findNext(this.offset).then(function (results) {
+        const chars = results.data.results
+        chars.forEach(function (item) {
+          this.characters.push(item)
+        }.bind(this))
+        this.offset++
+        this.busy = false
+      }.bind(this))
+    }.bind(this)
+  }
 
-		return def.promise;
-	};
-	const LoadMore = function($scope) {
-		this.offset = 0;
-		this.busy = false;
-		this.characters = [];
-		this.load = function() {
-			if(this.busy) {
-				return;
-			}
-			this.busy = true;
-			findNext(this.offset).then(function(results) {
-				var chars = results.data.results;
-				chars.forEach(function(item) {
-					this.characters.push(item);
-				}.bind(this));
-				this.offset++;
-				this.busy = false;
-			}.bind(this));
-		}.bind(this);
-	};
-
-	return {
-		find: find,
-		findOne: findOne,
-		LoadMore: LoadMore
-	};
+  return {
+    find: find,
+    findOne: findOne,
+    LoadMore: LoadMore
+  }
 }
 
-export {factoryName, factory}
+export { factoryName, factory }
